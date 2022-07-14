5709fd7f75249aea10a6e8b3687c17d358d715d0
Jcohen010 <justin.cohen010@gmail.com>
2022-07-14 13:47:47 -0400

    DAT-20 test

--- Extraction/ProductionDB/Fact_Selects.sql
+++ Extraction/ProductionDB/Fact_Selects.sql
@@ -5,8 +5,8 @@ DECLARE @UpperBound DATE = DATEADD(day, -1, @Today);
 -- Fact_Run_Event
 SELECT CostTransCreDate AS RunEventDate, CostTransCreTime AS RunEventTime, kShiftName AS RunEventShift, KJobCode AS JobNum, kAccCode AS CustomerID, QtyUp, kWorkcCode AS MachineID, CostTransCreUser AS EmployeeID, CostTransWCQty AS Quantity, CostTransHours AS RunEventHours, CostTransDownType AS DowntimeType, CostTransBaseVal AS RunEventValue
 FROM CostTrans
-WHERE CostTransCreDate BETWEEN @UpperBound AND @LowerBound;
--- Left Join to Fact_Job for QtyDies
+WHERE RunEventDate BETWEEN @UpperBound AND @LowerBound;
+-- Left Join to Fact_Job Customer and QtyUp
 
 
 
