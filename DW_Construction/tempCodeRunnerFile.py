0e9be203153d1d903dfee59e8b905cc793f33e3b
Jcohen010 <justin.cohen010@gmail.com>
2022-07-18 14:50:28 -0400

    Rename Fact selects file to align with data flow change

--- DW_Construction/tempCodeRunnerFile.py
+++ DW_Construction/tempCodeRunnerFile.py
@@ -4,13 +4,25 @@ from pandasql import sqldf
 mysql = lambda q: sqldf(q, globals())
 
 fact_rundtypes = {"JobID":str, "ItemID":str}
+fact_defecttypes = {"JobNum" : str}
 
 fact_run_event = pd.read_csv("Fact_Run_Event (TEST).csv", header = 0, encoding = 'unicode_escape', engine ='python' )
 fact_defective_case = pd.read_csv("Fact_Defective_Case (TEST).csv", header = 0, encoding = 'unicode_escape', engine ='python', dtype= str)
 
 fact_run_event = fact_run_event.astype(fact_rundtypes)
+fact_defective_case = fact_defective_case.astype(fact_defecttypes)
 
 print(mysql(
-"""SELECT JobID, MachineID, DepartmentID, EmployeeID, Quantity 
-FROM fact_run_event
-GROUP BY MachineID"""))
+"""
+WITH runbymachine AS(
+SELECT F.JobItem, F.MachineID, F.DepartmentID, Sum(Quantity) AS Quantity
+FROM fact_run_event as F
+GROUP BY MachineID, JobID)
+
+SELECT D.JobItem, D.DateFound,D.InspectShift, D.InspectGluer, D.DefectCode, D.DefectDesc, D.CaseQty, D.DefectiveSamples, D.TotalSamples, O.MachineID AS OriginMachineID, O.Quantity
+FROM fact_defective_case AS D
+INNER JOIN runbymachine AS O
+ON O.JobItem = D.JobItem
+"""))
+
+
