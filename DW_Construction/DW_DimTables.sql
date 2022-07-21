b02dc491abb532eda1242271b1afba111a0a0a29
Jcohen010 <justin.cohen010@gmail.com>
2022-07-21 10:49:07 -0400

    add "dbo." before Dim Table Names

--- DW_Construction/DW_DimTables.sql
+++ DW_Construction/DW_DimTables.sql
@@ -1,15 +1,15 @@
-CREATE TABLE Dim_Customer(
+CREATE TABLE dbo.Dim_Customer(
 CustomerID varchar(50) PRIMARY KEY,
 CustomerDesc varchar(50),
 )
 
-CREATE TABLE Dim_Machine(
+CREATE TABLE dbo.Dim_Machine(
 MachineID varchar(25) PRIMARY KEY,
 MachineDesc varchar(50),
 DepartmentID varchar(25) FOREIGN KEY REFERENCES Dim.Department.DepartmentID
 )
 
-CREATE TABLE Dim_Employee(
+CREATE TABLE dbo.Dim_Employee(
 EmployeeID VARCHAR(25) PRIMARY KEY,
 EmployeeName VARCHAR(100) NOT NULL,
 EmployeeEmail VARCHAR(50),
@@ -17,18 +17,18 @@ EmployeeTitle VARCHAR(50)
 )
 
 
-CREATE TABLE Dim_Shift(
+CREATE TABLE dbo.Dim_Shift(
 ShiftID VARCHAR(25) PRIMARY KEY NOT NULL,
 ShiftName VARCHAR(50)
 )
 
-CREATE TABLE Dim_Defect(
+CREATE TABLE dbo.Dim_Defect(
 DefectID VARCHAR(25) PRIMARY KEY NOT NULL,
 DefectDesc VARCHAR(50),
 DepartmentID VARCHAR(50) FOREIGN KEY REFERENCES Dim.Department.DepartmentID
 )
 
-CREATE TABLE Dim_Department(
+CREATE TABLE dbo.Dim_Department(
 DepartmentID VARCHAR(50) PRIMARY KEY NOT NULL,
 DepartmentDesc VARCHAR(50)
 )
