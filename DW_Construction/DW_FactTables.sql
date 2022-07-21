691a0aaca3b1b4a52b18dee7aa07bed7b5184d3d
Jcohen010 <justin.cohen010@gmail.com>
2022-07-21 10:51:02 -0400

    Add/Modify a few Fact Tables

--- DW_Construction/DW_FactTables.sql
+++ DW_Construction/DW_FactTables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Fact_Run_Event(
+CREATE TABLE dbo.Fact_Run_Event(
 ID int NOT NULL IDENTITY (1,1) PRIMARY KEY,
 RunEventDate DATE, 
 RunEventTime TIME, 
@@ -13,26 +13,20 @@ Quantity int,
 RunEventHours int,
 DowntimeType VARCHAR(30), 
 RunEventValue int
-)
+);
 
-CREATE TABLE Fact_FinGood(
+CREATE TABLE dbo.Fact_JobItem_Details(
+JobItemID VARCHAR(10),
 JobID VARCHAR(10),
 ItemID Varchar(20),
 CustomerID VARCHAR(25),
 ProdOrdQty Int,
-FinGoodQty Int, 
-QtyUp Int,
--- unfinished
-)
+FinGoodQty Int,
+DefectiveQty Int, 
+QtyUp Int
+);
 
-CREATE TABLE Fact_JobItem_Details(
-JobID VARCHAR(10),
-ItemID Varchar(20),
-CustomerID VARCHAR(25),
-
-)
-
-CREATE TABLE Fact_Defect_Event(
+CREATE TABLE dbo.Fact_Defect_Event(
 JobID VARCHAR(25),
 ItemID VARCHAR(25),
 JobItemID VARCHAR(25),
@@ -46,4 +40,18 @@ DefectiveSamples Int,
 TotalSamples Int, 
 OriginMachineID VARCHAR(25), 
 Quantity INT
+);
+
+CREATE TABLE dbo.Fact_NonConformingMaterial(
+NCRkey VARCHAR(25),
+JobID VARCHAR(25),
+CustomerID VARCHAR(25),
+DateID DATE,
+ReportedBy VARCHAR(25),
+LocationofFinding VARCHAR(25),
+OriginMachineID VARCHAR(25),
+SuspectedQty INT,
+UnitType VARCHAR(25),
+DefectID VARCHAR(25),
+Disposition VARCHAR(25),
 )
